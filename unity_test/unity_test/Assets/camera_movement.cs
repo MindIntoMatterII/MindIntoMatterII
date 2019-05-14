6064c8101fab9572d78588ff1826a9e7ccd2634c
feiooo <fei5@ualberta.ca>
2019-05-14 13:51:55 -0600

    Upload unity test

---
+++ unity_test/unity_test/Assets/camera_movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_movement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothTime = 0.1f;
+    public float speed = 3.0f;
+
+    public float followDistance = 10f;
+    public float verticalBuffer = 1.5f;
+    public float horizontalBuffer = 0f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    public Quaternion rotation = Quaternion.identity;
+
+    public float yRotation = 0.0f;
+
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(horizontalBuffer, followDistance, verticalBuffer));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        //this is the code that solves the problem
+        transform.eulerAngles = new Vector3(90, target.transform.eulerAngles.y, 0);
+        //------------------------------------------------------------------------
+    }
+}
