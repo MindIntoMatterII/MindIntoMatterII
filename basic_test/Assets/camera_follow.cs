7280b9fb2ff9690dcd4a6bd44e445ec098f8a817
feiooo <fei5@ualberta.ca>
2019-05-14 19:01:36 -0600

    Upload a simple test

---
+++ basic_test/Assets/camera_follow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Camera_follow : MonoBehaviour
+{
+    //摄像机跟离角色有多远
+    public float distanceAway;
+    //摄像机在角色上方多高
+    public float distanceUp;
+    //摄像机移动的速度
+    public float smooth;
+    //目标位置
+    private Vector3 targetPosition;
+    //摄像机所跟随的对象的位置,此处是角色物体的位置
+
+
+    public float rotationSpeed = 10;
+
+    Transform follow;
+    // Use this for initialization
+    void Start()
+    {
+        //得到要跟随的物体对象的位置
+        follow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey("q"))
+        //    print ("q key was pressed");
+        //    transform.Rotate(Vector3.up * 90 * rotationSpeed);
+        //if (Input.GetKey("e"))
+        //    print ("e key was pressed");
+        //    transform.Rotate(Vector3.down * 90 * rotationSpeed);
+
+        //得到摄像机要移动的目标位置
+        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+        //摄像机从当前位置移动到目标位置
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+        //摄像机要看向角色物体
+        transform.LookAt(follow);
+
+
+    }
+}
---
+++ basic_test/Assets/player_movement_1.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class player_movement_1 : MonoBehaviour
+{
+    private float speed = 10f;
+    private float jumpForce = 15f;
+    private float gravity = 30f;
+    private Vector3 moveDir = Vector3.zero;
+    private bool turnLeft;
+    private bool turnRight;
+    private float temp = 0f;
+    private float angle = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+
+        if (Input.GetKey("q"))
+        {
+            turnLeft = true;
+        }
+        else if (Input.GetKey("e"))
+        {
+            turnRight = true;
+        }
+
+
+    }
+
+    void LateUpdate()
+    {
+        if (turnLeft)
+        {
+            if (temp > 89)
+            {
+                turnLeft = false;
+                temp = 0f;
+            }
+            else
+            {
+                angle = 3;
+                transform.Rotate(new Vector3(0, angle, 0));
+                temp += angle;
+            }
+
+        }
+        else if (turnRight)
+        {
+            if (temp < -89)
+            {
+                turnRight = false;
+                temp = 0f;
+            }
+            else
+            {
+                angle = -3;
+                transform.Rotate(new Vector3(0, angle, 0));
+                temp += angle;
+            }
+
+        }
+    }
+}
