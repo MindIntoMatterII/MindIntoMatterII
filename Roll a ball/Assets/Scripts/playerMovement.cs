4694735af8944eafadb293e528b676e02584a609
feiooo <fei5@ualberta.ca>
2019-05-17 22:04:01 -0600

    Upload a new test game
    Collecting pickup and scoring can work

---
+++ Roll a ball/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+
+    public float speed;
+    public Text counterText;
+    public Text winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        
+    }
+
+    //collecting pickUp
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count =count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        counterText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+}
